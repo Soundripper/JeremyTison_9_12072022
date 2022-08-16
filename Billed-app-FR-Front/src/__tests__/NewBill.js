e6a9e8bff1981502f8f45d7cd729640d095c155e
Soundripper <jerrsoundripper@gmail.com>
2022-08-16 11:45:01 +0200

    NewBill test added router

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -15,7 +15,7 @@ import {waitFor} from "@testing-library/dom"
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import userEvent from "@testing-library/user-event"
-// import router from "../app/Router.js";
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -99,3 +99,74 @@ describe("Given I am connected as an employee", () => {
   })
 })
 
+
+
+
+
+// test d'intégration POST
+// describe("Given I am a user connected as Employee", () => {
+//   describe("When I navigate to NewBills", () => {
+//     test("fetches bills from mock API GET", async () => {
+//       localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "employee@test.tld" }));
+//       const root = document.createElement("div")
+//       root.setAttribute("id", "root")
+//       document.body.append(root)
+//       router()
+//       window.onNavigate(ROUTES_PATH.NewBill)
+//       await waitFor(() => screen.getByText("Envoyer une note de frais"))
+//       const newBillBtn  = await screen.getByText("Envoyer")
+//       expect(newBillBtn).toBeTruthy()
+//       const contentTest  = await screen.getAllByText("Justificatif")
+//       expect(contentTest).toBeTruthy()
+//     })
+
+  // describe("When an error occurs on API", () => {
+  //   beforeEach(() => {
+  //     jest.spyOn(mockStore, "bills")
+  //     Object.defineProperty(
+  //         window,
+  //         'localStorage',
+  //         { value: localStorageMock }
+  //     )
+  //     window.localStorage.setItem('user', JSON.stringify({
+  //       type: 'employee',
+  //       email: "employee@test.tld"
+  //     }))
+  //     const root = document.createElement("div")
+  //     root.setAttribute("id", "root")
+  //     document.body.appendChild(root)
+  //     router()
+  //   })
+
+  //   test("fetches bills from an API and fails with 404 message error", async () => {
+
+  //     mockStore.bills.mockImplementationOnce(() => {
+  //       return {
+  //         list : () =>  {
+  //           return Promise.reject(new Error("Erreur 404"))
+  //         }
+  //       }})
+  //     window.onNavigate(ROUTES_PATH.Bills)
+  //     await new Promise(process.nextTick);
+  //     const message = await screen.getByText(/Erreur 404/)
+  //     expect(message).toBeTruthy()
+  //   })
+
+  //   test("fetches messages from an API and fails with 500 message error", async () => {
+
+  //     mockStore.bills.mockImplementationOnce(() => {
+  //       return {
+  //         list : () =>  {
+  //           return Promise.reject(new Error("Erreur 500"))
+  //         }
+  //       }})
+
+  //     window.onNavigate(ROUTES_PATH.Bills)
+  //     await new Promise(process.nextTick);
+  //     const message = await screen.getByText(/Erreur 500/)
+  //     expect(message).toBeTruthy()
+  //   })
+  // })
+
+//   })
+// })
